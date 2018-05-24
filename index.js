ba75a396960e7f96ab4d05bf2ac340740f24129d
James Ferguson <ncjamesferguson@gmail.com>
2018-05-24 19:20:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function produceDrivingRange(range) {
   return function(block1, block2) {
 
     let distance = block2 - block1
-    return (distance > range ? false : true)
+    return (distance > range ? `${range - blockRange} blocks out of range` :`within range by ${blockRange - range}`)
   }
 
 }
